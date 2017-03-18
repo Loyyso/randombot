67be42ee1aa76943a415279e773ef5fe2bcc5986
Loyso <loyso.email@gmail.com>
2017-03-18 21:36:45 +0100

    Main class connects to JDA Added CommandListener class

---
+++ src/fr/loysofreezee/randombot/CommandListener.java
@@ -0,0 +1,13 @@
+package fr.loysofreezee.randombot;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class CommandListener extends ListenerAdapter {
+
+    public void onMessageRecieved(MessageReceivedEvent event) {
+        JDA jda = event.getJDA();
+        long responseNumber = event.getResponseNumber();
+    }
+}
--- src/fr/loysofreezee/randombot/Main.java
+++ src/fr/loysofreezee/randombot/Main.java
@@ -1,9 +1,32 @@
 package fr.loysofreezee.randombot;
 
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+
 public class Main {
+    public static JDA jda;
 
     public static void main(String args[]) {
-        System.out.println("Test");
-        System.exit(-1337);
+        //JDA connection
+        try {
+            jda = new JDABuilder(AccountType.BOT)
+                    .setToken("MjkyNzU1MzIwNTMzNTQ5MDY3.C68pNQ.bwsLdm4UVyDyLFhTWQXYtDnrX4E")
+                    .addListener(new CommandListener())
+                    .buildBlocking();
+        }
+        catch(LoginException e){                //Authentication Problem
+            e.printStackTrace();
+            System.out.println("Connection failed");
+        }
+        catch(InterruptedException e){          //buildBlocking waiting interrupted
+            e.printStackTrace();
+        }
+        catch(RateLimitedException e) {          //Too much simultaneous logins
+            e.printStackTrace();
+        }
     }
 }
